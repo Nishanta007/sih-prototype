9071be08227196b4b985c80cfad6a760a2f3ede3
Rahul Singh <109950602+RahulSingh693@users.noreply.github.com>
2023-09-26 02:14:48 +0530

    Update Dashboard.jsx

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Dashboard.css';
-import Sidebar from '../MyComponents/SideBar';
+import List_phase1 from '../MyComponents/List_phase1';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import PendingActionsIcon from '@mui/icons-material/PendingActions';
+import CancelScheduleSendIcon from '@mui/icons-material/CancelScheduleSend';
 
 const Dashboard = () => {
+  const [showListPhase1, setShowListPhase1] = useState(false);
+  const handleVerificationRequestsClick = () => {
+    setShowListPhase1(!showListPhase1);
+  };
   return (
     <>
-      <div >
-        <Sidebar />
+      <div className="contain">
+        <div className="side">
+          <List>
+            <ListItem disablePadding className='list_elem' onClick={handleVerificationRequestsClick}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <PendingActionsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Verification Requests" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding className='list_elem'>
+              <ListItemButton>
+                <ListItemIcon>
+                  <HowToRegIcon />
+                </ListItemIcon>
+                <ListItemText primary="Verified" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding className='list_elem'>
+              <ListItemButton>
+                <ListItemIcon>
+                  <CancelScheduleSendIcon />
+                </ListItemIcon>
+                <ListItemText primary="Rejcted Requests" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </div>
+        {showListPhase1 && <List_phase1 />}
       </div>
     </>
   )
