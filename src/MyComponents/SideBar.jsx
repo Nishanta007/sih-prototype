b7405d163ebb7a0b2427eeceb00dabc459502413
Rahul Singh <109950602+RahulSingh693@users.noreply.github.com>
2023-09-26 02:15:22 +0530

    Update SideBar.jsx

--- src/MyComponents/SideBar.jsx
+++ src/MyComponents/SideBar.jsx
@@ -1,26 +1,19 @@
 import React, { useState } from 'react';
 import '../Pages/Dashboard.css';
-import List_phase1 from './List_phase1';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import HowToRegIcon from '@mui/icons-material/HowToReg';
 import PendingActionsIcon from '@mui/icons-material/PendingActions';
-import CancelScheduleSendIcon from '@mui/icons-material/CancelScheduleSend';
 
 const SideBar = () => {
-  const [showListPhase1, setShowListPhase1] = useState(false);
-  const handleVerificationRequestsClick = () => {
-    setShowListPhase1(!showListPhase1);
-  };
   return (
     <>
       <div className="contain">
         <div className="side">
           <List>
-            <ListItem disablePadding className='list_elem' onClick={handleVerificationRequestsClick}>
+            <ListItem disablePadding className='list_elem'>
               <ListItemButton>
                 <ListItemIcon>
                   <PendingActionsIcon />
@@ -28,25 +21,8 @@ const SideBar = () => {
                 <ListItemText primary="Verification Requests" />
               </ListItemButton>
             </ListItem>
-            <ListItem disablePadding className='list_elem'>
-              <ListItemButton>
-                <ListItemIcon>
-                  <HowToRegIcon />
-                </ListItemIcon>
-                <ListItemText primary="Verified" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding className='list_elem'>
-              <ListItemButton>
-                <ListItemIcon>
-                  <CancelScheduleSendIcon />
-                </ListItemIcon>
-                <ListItemText primary="Rejcted Requests" />
-              </ListItemButton>
-            </ListItem>
           </List>
         </div>
-        {showListPhase1 && <List_phase1 />}
       </div>
     </>
   )
