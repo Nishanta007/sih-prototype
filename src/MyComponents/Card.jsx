a6779ee84fa6b2756051420cc252d627b86cb663
NIshanta <134363526+Nishanta007@users.noreply.github.com>
2023-09-26 01:40:26 +0530

    Update Card.jsx

--- src/MyComponents/Card.jsx
+++ src/MyComponents/Card.jsx
@@ -3,24 +3,28 @@ import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import "./organization.css";
 import EditIcon from '@mui/icons-material/Edit';
+import EditNoteTwoToneIcon from '@mui/icons-material/EditNoteTwoTone';
 import TaskAltIcon from '@mui/icons-material/TaskAlt';
 
 const Card = (props) => {
     return (
         <>
             <div className="cards-Contain">
-                <div className="detail">
-                    {props.name}
+                <div>
+                    {props.key}{props.name}
                 </div>
                 <div className="bttn-bar">
-                    <Button className="bttn1" variant="contained" endIcon={<EditIcon />}>Edit</Button>
-                    <Button className="bttn2" variant="contained" color="success" endIcon={<TaskAltIcon />}>Fill</Button>
-                    <Button className="bttn3" variant="contained"
-                        onClick={() => props.fun(props.key)}
-                        startIcon={<DeleteIcon />}
-                    >Delete</Button>
+                    {/* <Button className="bttn1" variant="contained" endIcon={<EditIcon />}>Edit</Button> */}
+
+                    <Button className="bttn2" variant="outlined" color="primary">Register Student</Button>
+                    <Button className="bttn2" variant="outlined" color="primary">Registered Students</Button>
+                    {/* <EditNoteTwoToneIcon  style={{ cursor: 'pointer', color: 'black' }} />Edit */}
+                    <DeleteIcon style={{ cursor: 'pointer', color: 'black' }} onClick={() => props.fun(props.key)}/>
+
+                    {/* <Button className="bttn3" variant="contained" onClick={() => props.fun(props.key)} startIcon={<DeleteIcon />}>Delete</Button> */}
                 </div>
             </div>
+                <hr></hr>
         </>
     )
 }
